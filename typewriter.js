abd6b5705d8fd03700b0b28cbc817d43b890f8d6
tomineodegard-kea <tomi0105@stud.kea.dk>
2022-02-08 16:01:05 +0100

    typewriter is working

--- typewriter.js
+++ typewriter.js
@@ -1,38 +1,24 @@
 "use strict";
 // exercise 08.02.22, creating an effect where the text is typed out one letter at the time
 
-let maxLength;
 let iterator;
+let text = document.querySelector("#typewriter").textContent;
+document.querySelector("#typewriter").textContent = "";
 
-// step 1: page load
-window.addEventListener("load", loadFunction);
-
-function loadFunc() {
-  console.log("loadFunc function working");
-  setUp();
-}
-
-function setUp() {
-  // step 2: get the text
-  console.log("setUp function working");
-  initTextLoop();
-}
+window.addEventListener("load", initTextLoop);
 
 function initTextLoop() {
   console.log("initTextLoop function working");
   iterator = 0;
-  maxLength = 10;
   textLoop();
 }
 
 function textLoop() {
-  iterator++;
   console.log("i am looping");
-  if (iterator < maxLength) {
-    textLoop();
+
+  if (iterator < text.length) {
+    document.querySelector("#typewriter").textContent += text.charAt(iterator);
+    iterator++;
+    setTimeout(textLoop, 500);
   }
 }
-
-// step 3: show the text, one at the time
-// step 4:
-// step 5:
